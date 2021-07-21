36c8c31f34fc84fb2ac2bf7f650c82e8afbe7ca5
vijai19 <vijayselvaraj19@gmail.com>
2021-07-21 16:12:09 +0800

    project

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,46 @@
+package org.add;
+
+public class GreensTech {
+
+	public void greensOmr() {
+
+		System.out.println("Greens Omr in duraipakkam");
+
+	}
+	
+	public void greensAdayar() {
+
+		System.out.println("Greens Adayar in adayar");
+		
+	}
+	
+	public void greensTambaram() {
+
+		System.out.println("Greens Tambaram in Tambarm");
+		
+	}
+	
+	public void greensVelachery() {
+
+		System.out.println("Greens Velachery in velachery");
+		
+	}
+	
+	public void greensAnnaNagar() {
+
+		System.out.println("Greens Annanagar in AnnaNagar");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		GreensTech e = new GreensTech();
+		
+		e.greensOmr();
+		e.greensAdayar();
+		e.greensTambaram();
+		e.greensVelachery();
+		e.greensAnnaNagar();
+	}
+	
+}
